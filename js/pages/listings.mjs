ead37b066e8dfb0e2d0e80b5ff9c5ba2a8d0fca2
VeronicaOS <verols51029@stud.noroff.no>
2024-05-11 18:23:10 +0200

    testing

--- js/pages/listings.mjs
+++ js/pages/listings.mjs
@@ -85,17 +85,7 @@ listings.forEach((listing) => {
     listingsContainer.innerHTML += renderListing(listing);
 });
 
-// save scroll
 
-var $window = $(window)
-
-/* Restore scroll position */
-window.scroll(0, localStorage.getItem('scrollPosition')|0)
-
-/* Save scroll position */
-$window.scroll(function () {
-	localStorage.setItem('scrollPosition', $window.scrollTop())
-})
 
 // const sorting = document.getElementById("sort-by");
 // sorting.addEventListener("change", function (event) {
